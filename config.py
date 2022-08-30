601c5348d4eb614e47139b7cf5f04284e74d5d5a
Serg <89090168690@mail.ru>
2022-08-30 20:51:10 +0500

    разбивка сообщений пользователей

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['89090168690@mail.ru']
+    POSTS_PER_PAGE = 20
